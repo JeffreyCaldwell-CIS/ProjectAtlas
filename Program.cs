61e3a1c6341d922b95be5d013d2f591575ace2cc
Jeffrey Caldwell <jeffrey.caldwell.cis@gmail.com>
2025-11-25 20:59:11 -0500

    Initial project structure

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core4Logistics
+{
+    //Entry point of the program
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Create the main driver list
+            List<Driver> drivers = new List<Driver>();
+
+           //Launch the main menu
+           MainMenu.Show(drivers, loads);
+        }
+    }
+}
